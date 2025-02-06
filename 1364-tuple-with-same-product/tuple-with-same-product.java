3b1f6bfa27ffaf68cee1852c12e5b92087cec030
Devansh Agarwal <68228581+DevanshAgarwal7@users.noreply.github.com>
2025-02-07 00:24:58 +0530

    Time: 224 ms (52.56%) | Memory: 80.2 MB (12.18%) - LeetSync

---
+++ 1364-tuple-with-same-product/tuple-with-same-product.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int tupleSameProduct(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int result = 0;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int product = nums[i] * nums[j];
+                result += map.getOrDefault(product, 0) * 8;
+                map.put(product, map.getOrDefault(product, 0) + 1);
+            }
+        }
+        return result;
+    }
+}
