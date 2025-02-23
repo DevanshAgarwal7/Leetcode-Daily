8f9edb8878b8450c4ba9bec50c267570ab6d4513
Devansh Agarwal <68228581+DevanshAgarwal7@users.noreply.github.com>
2025-02-23 23:43:17 +0530

    Time: 7 ms (42.54%) | Memory: 44.9 MB (54.85%) - LeetSync

--- 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.java
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.java
@@ -1,47 +1,85 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.Iterator;
+
 class Solution {
-    public TreeNode recoverFromPreorder(String s) {
-        List<int[]> vp = new ArrayList<>();
-        int i = 0;
-        while (i < s.length()) {
-            int depth = 0;
-            while (i < s.length() && s.charAt(i) == '-') {
-                depth++;
-                i++;
-            }
-            int num = 0;
-            while (i < s.length() && s.charAt(i) != '-') {
-                num = num * 10 + (s.charAt(i) - '0');
-                i++;
-            }
-            vp.add(new int[]{depth, num});
+    public TreeNode recoverFromPreorder(String traversal) {
+        //ye mene khud code kiya h
+        Map<Integer, Stack<TreeNode>> levelMap = new HashMap<>();
+        int lastIndexWhereValuePresent = 0;
+        int n = traversal.length();
+        int nodeValue = 0;
+        while(lastIndexWhereValuePresent < n && traversal.charAt(lastIndexWhereValuePresent) != '-'){
+            nodeValue = nodeValue * 10 + (traversal.charAt(lastIndexWhereValuePresent) - '0');
+            ++lastIndexWhereValuePresent;
         }
+        //System.out.println(value);
+        int currentIndex = lastIndexWhereValuePresent;
+        //ye minus islie kra kyuki while loop ke ye '-' ko point kr raha tha aur hame chaia last nodeVlaue ki last digit ki position.
+        --lastIndexWhereValuePresent;
+        TreeNode root = new TreeNode(nodeValue);
+        levelMap.put(0, new Stack<>());
+        levelMap.get(0).push(root);
 
-        Stack<Pair<Integer, TreeNode>> st = new Stack<>();
-        TreeNode root = null;
-        
-        for (int[] it : vp) {
-            int depth = it[0];
-            int nodeVal = it[1];
-            TreeNode node = new TreeNode(nodeVal);
-
-            while (!st.isEmpty() && st.peek().getKey() >= depth) {
-                st.pop();
-            }
-
-            if (!st.isEmpty()) {
-                TreeNode parent = st.peek().getValue();
-                if (parent.left == null) {
-                    parent.left = node;
-                } else {
-                    parent.right = node;
+        while(currentIndex < n){
+            if(traversal.charAt(currentIndex) != '-'){
+                nodeValue = 0;
+                int startIndex = currentIndex;
+                while(startIndex < n && traversal.charAt(startIndex) != '-'){
+                    nodeValue = (nodeValue *= 10) + (traversal.charAt(startIndex) - '0');
+                    ++startIndex;
                 }
-            } else {
-                root = node;
+                //System.out.println(nodeValue);
+                int levelAtWhichNodeWillInsert = currentIndex - lastIndexWhereValuePresent - 1;
+                TreeNode node = new TreeNode(nodeValue);
+                insertNodeInTree(node, levelAtWhichNodeWillInsert-1, levelMap);
+                levelMap.putIfAbsent(levelAtWhichNodeWillInsert, new Stack<>());
+                levelMap.get(levelAtWhichNodeWillInsert).push(node);
+                lastIndexWhereValuePresent = startIndex-1;
+                currentIndex = startIndex;
+            } else{
+                ++currentIndex;
             }
-
-            st.push(new Pair<>(depth, node));
         }
-        
         return root;
+    }
+    private void insertNodeInTree(TreeNode node, int parentLevelOfNewNode, Map<Integer, Stack<TreeNode>> levelMap){
+        Stack<TreeNode> nodesAtPerticularLevel = levelMap.get(parentLevelOfNewNode);
+        //System.out.println(node.val);
+        while (!nodesAtPerticularLevel.isEmpty()) {
+            TreeNode toBeParent = nodesAtPerticularLevel.peek();
+            //System.out.println(toBeParent.val);
+            if(canBeParentOfNode(toBeParent)){
+                if(toBeParent.left == null){
+                    toBeParent.left = node;
+                } else{
+                    toBeParent.right = node;
+                }
+                return;
+            } else{
+                nodesAtPerticularLevel.pop();
+            }
+            // if(isBothChildExistOfToBeParent(toBeParent)){
+            // }
+        }
+    }
+    private boolean canBeParentOfNode(TreeNode parent){
+        return parent.left == null || parent.right == null;
+    }
+    private boolean isBothChildExistOfToBeParent(TreeNode parent){
+        return parent.left != null && parent.right != null;
     }
 }
