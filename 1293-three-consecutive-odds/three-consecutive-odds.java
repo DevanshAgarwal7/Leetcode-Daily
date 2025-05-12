6d1910828f66d18f633b3e8e931e6ea7989d2448
Devansh Agarwal <68228581+DevanshAgarwal7@users.noreply.github.com>
2025-05-12 21:56:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (55.12%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.java
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -1,14 +1,10 @@
 class Solution {
-
     public boolean threeConsecutiveOdds(int[] arr) {
-        // Loop through the array up to the third-to-last element
-        for (int i = 0; i < arr.length - 2; i++) {
-            // Check if the current element and the next two elements are all odd
-            if (arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1) {
+        for(int i=0;i<arr.length-2;i++){
+            if(arr[i] % 2 == 1 && arr[i+1] % 2 == 1 && arr[i+2] % 2 == 1){
                 return true;
             }
         }
-
         return false;
     }
 }
