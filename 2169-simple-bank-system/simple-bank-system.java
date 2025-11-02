694bc41607052693a0bf07205c75bd9f5b4059a0
Devansh Agarwal <68228581+DevanshAgarwal7@users.noreply.github.com>
2025-11-02 16:08:31 +0530

    Time: 96 ms (84.76%) | Memory: 123.8 MB (7.64%) - LeetSync

--- 2169-simple-bank-system/simple-bank-system.java
+++ 2169-simple-bank-system/simple-bank-system.java
@@ -1,37 +1,41 @@
 class Bank {
-
-    long[] balance;
-
+    private long[] balance;
+    private int n;
     public Bank(long[] balance) {
         this.balance = balance;
+        this.n = balance.length;
     }
-
+    
     public boolean transfer(int account1, int account2, long money) {
-        if (
-            account1 > balance.length ||
-            account2 > balance.length ||
-            balance[account1 - 1] < money
-        ) {
-            return false;
+        if(account1 <= n && balance[account1-1] >= money && account2 <= n){
+            balance[account1-1] -= money;
+            balance[account2-1] += money;
+            return true;
         }
-        balance[account1 - 1] -= money;
-        balance[account2 - 1] += money;
-        return true;
+        return false;
     }
-
+    
     public boolean deposit(int account, long money) {
-        if (account > balance.length) {
-            return false;
+        if(account <= n){
+            balance[account-1] += money;
+            return true;
         }
-        balance[account - 1] += money;
-        return true;
+        return false;
     }
-
+    
     public boolean withdraw(int account, long money) {
-        if (account > balance.length || balance[account - 1] < money) {
-            return false;
+        if(account <= n && balance[account-1] >= money){
+            balance[account-1] -= money;
+            return true;
         }
-        balance[account - 1] -= money;
-        return true;
+        return false;
     }
 }
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank obj = new Bank(balance);
+ * boolean param_1 = obj.transfer(account1,account2,money);
+ * boolean param_2 = obj.deposit(account,money);
+ * boolean param_3 = obj.withdraw(account,money);
+ */
