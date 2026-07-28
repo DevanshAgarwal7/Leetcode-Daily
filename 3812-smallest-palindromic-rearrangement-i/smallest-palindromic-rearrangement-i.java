bca68887e340605a98c096d8e2f4c90482cd2a04
Devansh Agarwal <68228581+DevanshAgarwal7@users.noreply.github.com>
2026-07-28 21:25:52 +0530

    Time: 158 ms (7.23%) | Memory: 53.9 MB (10.04%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -1,40 +1,16 @@
 class Solution {
     public String smallestPalindrome(String s) {
-         int n = s.length();
+        int n = s.length();
         if(n <= 1){
             return s;
         }
-        int[] freq = new int[26];
-        for(char ch: s.toCharArray()){
-            freq[ch - 'a']++;
+        int half = (int) Math.ceil(n/2);
+        String str = s.substring(0, half);
+        String result = str.chars().sorted().mapToObj(ch -> String.valueOf((char) ch)).collect(Collectors.joining());
+        StringBuilder sb = new StringBuilder(result);
+        if(n%2 == 1){
+            result += s.charAt(n/2);
         }
-        int oddCount = 0;
-        char oddCh = 0;
-        for(int i=0;i<26;i++){
-            if(freq[i] % 2 != 0){
-                oddCount++;
-                oddCh = (char)(i + 'a');
-            }
-        }
-        if(oddCount > 1){
-            return "";
-        }
-        char[] result = new char[n];
-        int left = 0;
-        int right = n-1;
-        for(int i=0;i<26;i++){
-            if(freq[i] != 0){
-                char ch = (char)(i + 'a');
-                while(freq[i] >= 2){
-                    result[left++] = ch;
-                    result[right--] = ch;
-                    freq[i] -= 2;
-                }
-            }
-        }
-        if(oddCount == 1){
-            result[left] = oddCh;
-        }
-        return new String(result);
+        return result + sb.reverse().toString();
     }
 }
