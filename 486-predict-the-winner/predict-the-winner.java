52e9d18afa279c30c3cd8d48e7da12a5f74be536
Devansh Agarwal <68228581+DevanshAgarwal7@users.noreply.github.com>
2026-08-02 00:36:22 +0530

    Time: 4 ms (33.51%) | Memory: 43.1 MB (10.58%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,7 +1,9 @@
 class Solution {
+    private int[][] memo = new int[23][23];
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
         int sum = Arrays.stream(nums).sum();
+        Arrays.stream(memo).forEach(arr -> Arrays.fill(arr, -1));
         int scoreOfP1 = solveForP1(0, n-1, nums);
         int scoreOfP2 = sum - scoreOfP1;
         return scoreOfP1 >= scoreOfP2;
@@ -14,9 +16,12 @@ class Solution {
         if(i==j){
             return nums[i];
         }
+        if(memo[i][j] != -1){
+            return memo[i][j];
+        }
         int takeI = nums[i] + Math.min(solveForP1(i+2, j, nums), solveForP1(i+1, j-1, nums));
         int takeJ = nums[j] + Math.min(solveForP1(i+1, j-1, nums), solveForP1(i, j-2, nums));
 
-        return Math.max(takeI, takeJ);
+        return memo[i][j] = Math.max(takeI, takeJ);
     }
 }
